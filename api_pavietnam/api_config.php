21029d6b704168fb34a47a4c8235e3742ca82fc6
hoangsoft90 <hoangsoft90@gmail.com>
2016-02-06 21:38:25 +0700

    sdsfdf

--- api_pavietnam/api_config.php
+++ api_pavietnam/api_config.php
@@ -6,8 +6,8 @@
 			- Link API test: https://daily.pavietnam.vn/interface_test.php
 			- Link API đăng ký thật: https://daily.pavietnam.vn/interface.php
 	*/
-	define('USERNAME','hoangweb');//Username đại lý
-	define('API_KEY','ca660b3417f5127e14ba1d3795b1af7b');//API KEY
+	define('USERNAME','xxxxx');//Username đại lý
+	define('API_KEY','ca660b3417f5127e14basd31d3795b1af7b');//API KEY
 	define('API_URL','https://daily.pavietnam.vn/interface_test.php');//Link API (Mặc định đang là link API test)
 	
 ?>
